8ac30534c37c1f350a25697f6d120526ed27eb17
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2022-12-28 17:12:07 -0300

    Fieldset

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, EventHandler, ChangeEvent } from "react";
 import { Post } from "./types/Post";
 
 const App = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(false);
+  const [addTitleText, setAddTitleText] = useState("");
+  const [addBodyText, setAddBodyText] = useState("");
 
   useEffect(() => {
     loadPosts();
@@ -34,9 +36,37 @@ const App = () => {
     setPosts(json);
   };
 
+  const handleAddTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setAddTitleText(e.target.value);
+  };
+
+  const handleAddBodyChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setAddBodyText(e.target.value);
+  };
+
+  const handleAddClick = () => {};
+
   return (
     <div className="p-5">
       {loading && <div>Carregando...</div>}
+      <fieldset className="border-2 mb-3 p-3">
+        <legend>Adicionar novo post</legend>
+        <input
+          value={addTitleText}
+          onChange={handleAddTitleChange}
+          className="block border"
+          type="text"
+          placeholder="Digite um título"
+        />
+        <textarea
+          value={addBodyText}
+          onChange={handleAddBodyChange}
+          className="block border"
+        ></textarea>
+        <button className="block border" onClick={handleAddClick}>
+          Adicionar
+        </button>
+      </fieldset>
       {!loading && posts.length > 0 && (
         <>
           <p>Total de posts: {posts.length}</p>
