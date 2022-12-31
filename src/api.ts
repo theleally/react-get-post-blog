05e3b4b3294485c587fab27c89fab1ec0f8c0ab2
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2022-12-31 17:47:21 -0300

    Using axios

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,26 @@
-const BASE = "https://jsonplaceholder.typicode.com";
+import axios from "axios";
+
+/*const BASE = "https://jsonplaceholder.typicode.com";*/
+const http = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
 
 export const api = {
   getAllPosts: async () => {
+    /*let response = await axios.get(`${BASE}/posts`);*/
+    let response = await http.get("/posts");
+    return response.data;
+    /*
     let response = await fetch(`${BASE}/posts`);
     let json = await response.json();
     return json;
+    */
   },
   addNewPost: async (title: string, body: string, userId: number) => {
+    /*let response = await axios.post(`${BASE}/posts`, { title, body, userId });*/
+    let response = await http.post("/posts", { title, body, userId });
+    return response.data;
+    /*
     let response = await fetch(`${BASE}/posts`, {
       method: "POST",
       body: JSON.stringify({ title, body, userId }),
@@ -14,5 +28,6 @@ export const api = {
     });
     let json = await response.json();
     return json;
+    */
   },
 };
